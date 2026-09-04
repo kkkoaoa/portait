6622b64125084f0e9468b58a0f2b29888cbd988e
wang shali <85485038+kkkoaoa@users.noreply.github.com>
2026-09-04 14:08:32 +0800

    Initial portfolio site

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+export default { content: ['./index.html','./src/**/*.{js,jsx}'], theme: { extend: { fontFamily: { sans:['"Noto Sans SC"','sans-serif'], display:['"Noto Sans SC"','sans-serif'] } } }, plugins: [] };
